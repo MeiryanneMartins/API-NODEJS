c516be855ca0bafa4ad985338fd098d083dfa90a
Meiryanne Martins <meirymartinsp.rodrigues@gmail.com>
2022-10-04 17:05:20 -0300

    Configuring and testing sequelize

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,8 +1,17 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    // criando um novo objeto e retornando em json
+    const novoAluno = await Aluno.create({
+      nome: 'Meiryanne',
+      sobrenome: 'Martins',
+      email: 'mey@gmail.com',
+      idade: 29,
+      peso: 50,
+      altura: 1.62,
     });
+    res.json(novoAluno);
   }
 }
 
