2981948fe41210e9c1f5f664b49f633a1a6fe29a
Meiryanne Martins <meirymartinsp.rodrigues@gmail.com>
2022-10-16 12:49:58 -0300

    Fixes security

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,7 +5,9 @@ class UserController {
     try {
       // criando um novo objeto e retornando em json
       const novoUser = await User.create(req.body);
-      return res.json(novoUser);
+      // aqui é para mostrar somente esses campos
+      const { id, nome, email } = novoUser;
+      return res.json({ id, nome, email });
     } catch (e) {
       return res.status(400).json(e.errors.map((err) => err.message));
     }
@@ -13,7 +15,7 @@ class UserController {
 
   async index(req, res) {
     try {
-      const users = await User.findAll();
+      const users = await User.findAll({ attributes: ['id', 'nome', 'email'] });
       return res.json(users);
     } catch (e) {
       return res.json(null);
@@ -23,7 +25,8 @@ class UserController {
   async show(req, res) {
     try {
       const user = await User.findByPk(req.params.id);
-      return res.json(user);
+      const { id, nome, email } = user;
+      return res.json({ id, nome, email });
     } catch (e) {
       return res.json(null);
     }
@@ -37,7 +40,7 @@ class UserController {
         });
       }
 
-      const user = await User.findByPk(req.params.id);
+      const user = await User.findByPk(req.userId);
 
       if (!user) {
         return res.status(400).json({
@@ -46,7 +49,8 @@ class UserController {
       }
 
       const novosDados = await user.update(req.body);
-      return res.json(novosDados);
+      const { id, nome, email } = novosDados;
+      return res.json({ id, nome, email });
     } catch (e) {
       return res.status(400).json({
         errors: e.errors.map((err) => err.message),
@@ -62,7 +66,7 @@ class UserController {
         });
       }
 
-      const user = await User.findByPk(req.params.id);
+      const user = await User.findByPk(req.userId);
 
       if (!user) {
         return res.status(400).json({
