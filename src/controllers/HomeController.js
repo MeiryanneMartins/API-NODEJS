115aa1e8fbdbf549b1bd4744a01fce786565666f
Meiryanne Martins <meirymartinsp.rodrigues@gmail.com>
2022-10-17 18:57:44 -0300

    Create router alunos

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,17 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    // criando um novo objeto e retornando em json
-    const novoAluno = await Aluno.create({
-      nome: 'Meiryanne',
-      sobrenome: 'Martins',
-      email: 'mey@gmail.com',
-      idade: 29,
-      peso: 50,
-      altura: 1.62,
-    });
-    res.json(novoAluno);
+    res.json('index');
   }
 }
 
