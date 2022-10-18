6b000d6bb1c40fde48ccc18d3f2001a020fc0a9e
Meiryanne Martins <meirymartinsp.rodrigues@gmail.com>
2022-10-18 16:31:12 -0300

    Handling uploaded files

--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -1,13 +1,8 @@
 import { Router } from 'express';
-import multer from 'multer';
-
-import multerConfig from '../config/multerConfig';
 import fotoController from '../controllers/FotoController';
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.post('/', upload.single('foto'), fotoController.create);
+router.post('/', fotoController.create);
 
 export default router;
